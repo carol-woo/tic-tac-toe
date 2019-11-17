163bcc537e917045aa506daf07df91a244437944
Araxiah <cewoo98@hotmail.com>
2019-11-17 16:09:19 -0500

    added players component and css for players

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import './App.css';
-import box from './components/box'
-import game from './components/game'
-import players from './components/players'
+import Box from './components/Box'
+import Game from './components/Game'
+import Players from './components/Players'
 
 function App() {
   return (
     <div className="App">
-     <p>Hello Wurld c:</p>
-    
-     <div className="top-left"></div>
-     <div className="top-middle"></div>
-     <div className="top-right"></div>
-
-     <div classname="middle-left"></div>
-     <div className="middle"></div>
-     <div className="middle-right"></div>
-
-     <div className="bottom=left"></div>
-     <div className="bottom-middle"></div>
-     <div className="bottom-right"></div>
-
+      <p>Hello Wurld c:</p>
+      <Players/>
     </div>
   );
 }
--- src/components/Players.js
+++ src/components/Players.js
@@ -1,1 +1,12 @@
-import react from "react"
+import React, {Fragment} from "react"
+
+
+export default function Players(props) {
+  
+  return (
+    <div className='players'>
+      <div className='player'>Player 1</div>
+      <div className='player'>Player 2</div>
+    </div>
+  )
+}
